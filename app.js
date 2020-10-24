371170bf9a87765e6b40f5cc1572782b99fb9879
Hugo <carnielhugo8@gmail.com>
2020-10-24 13:25:09 -0300

    add in localhost

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
+app.use("/public", express.static("public"));
 
 app.listen(port);
